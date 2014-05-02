998e34aa5622156826489caa2885f9f4308dea6f
Mariano Benedettini <mariano.benedettini@devecoop.com>
2014-05-02 16:05:25 -0300

    Render home view into a main container

--- app/scripts/application.js
+++ app/scripts/application.js
@@ -1,20 +1,20 @@
+'use strict';
 define(function(require) {
     var Backbone = require('backbone'),
         Communicator = require('communicator'),
-        Welcome_tmpl = require('hbs!tmpl/welcome');
-    'use strict';
-
-	var welcomeTmpl = Welcome_tmpl;
+        HomeView = require('views/home');
 
 	var App = new Backbone.Marionette.Application();
 
 	/* Add application regions here */
-	App.addRegions({});
+	App.addRegions({
+        mainRegion: '#main-region'
+    });
 
 	/* Add initializers here */
 	App.addInitializer( function () {
-		document.body.innerHTML = welcomeTmpl({ success: "CONGRATS!" });
 		Communicator.mediator.trigger("APP:START");
+        App.mainRegion.show(new HomeView());
 	});
 
 	return App;
--- app/scripts/views/home.js
+++ app/scripts/views/home.js
@@ -1,12 +1,16 @@
-define([
-	'backbone'
-],
-function(Backbone){
-    'use strict';
+'use strict';
+define(function(require) {
+    var Backbone = require('backbone'),
+        HomeTpl = require('hbs!tmpl/home');
 
 	return Backbone.View.extend({
 		initialize: function() {
 			console.log("initialize a Home View");
-		}
+		},
+
+        render: function() {
+            this.$el.html(HomeTpl());
+            return this;
+        }
 	});
 });
